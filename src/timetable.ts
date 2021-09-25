5f958f662c9bd0a447e3183784d637160f5a3539
Tch1b0 <Jpour3006@gmail.com>
2021-09-25 16:53:01 +0200

    switched over to cheerio

--- src/timetable.ts
+++ src/timetable.ts
@@ -1,71 +1,43 @@
 import { Entry } from "./entry";
-import cheerio from "cheerio";
+import cheerio, { Cheerio } from "cheerio";
 
 export class TimeTable {
 	constructor(public readonly entries: Array<Entry>) {}
 
-	static fromHtml(html: string) {
-		const $ = cheerio.load(html);
+	static fromHtml(raw_html: string) {
+		const $ = cheerio.load(raw_html);
 
+		let centers = $("center");
 		var entries: Array<Entry> = [];
 
-		for (var i = 0; i < $("center").length; i++) {
-			let centerTag = $("center").l[i];
-
-			console.log("CHECKPOINT 1 - " + centerTag.firstChild);
-
-			if (!centerTag.firstChild || centerTag.children[0] !== "div") {
+		for (let center of centers) {
+			if (center.children.length <= 1) {
 				continue;
 			}
+			var day = $(center)
+				.find(".mon_title")
+				.text()
+				.replace(",", "")
+				.split(" ")[1];
 
-			var day: string = "";
+			for (let row of $(center).find("tr")) {
+				if ($(row).find("th").length !== 0) continue;
 
-			for (var j = 0; j < centerTag.children.length; j++) {
-				console.log("CHECKPOINT 2");
+				let columns = $(row).find("td");
 
-				let child = centerTag.children[j];
-				let childClass = child.className;
-
-				// <div class="mon_title">
-				if (childClass === "mon_title") {
-					let raw_text = centerTag.children[j].innerHTML;
-					let text = raw_text.split(" ");
-					day = text[1];
-				}
-
-				// <div class="mon_list">
-				if (childClass === "mon_list") {
-					console.log("CHECKPOINT 3");
-
-					// <tbody>
-					let schedule = child.children[0];
-
-					// <tr class="list ...">
-					for (var e = 0; e < schedule.children.length; e++) {
-						let rawEntry = schedule.children[e];
-
-						if (rawEntry.classList.length == 1) {
-							continue;
-						}
-
-						// Entry Children
-						let ec = rawEntry.children;
-
-						entries.push(
-							new Entry(
-								day,
-								ec[0].innerHTML.split(" "),
-								ec[1].innerHTML,
-								ec[2].innerHTML,
-								ec[3].innerHTML,
-								ec[4].innerHTML,
-								ec[5].innerHTML,
-								ec[6].innerHTML,
-								ec[7].innerHTML
-							)
-						);
-					}
-				}
+				entries.push(
+					new Entry(
+						day,
+						$(columns[0]).text().split(" "),
+						$(columns[1]).text(),
+						$(columns[2]).text(),
+						$(columns[3]).text(),
+						$(columns[4]).text(),
+						$(columns[5]).text(),
+						$(columns[6]).text(),
+						$(columns[7]).text()
+					)
+				);
 			}
 		}
 
