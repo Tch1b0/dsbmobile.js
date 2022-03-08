ac1c7155caa0918cb1e2a1126614ace60c6aef8e
Tch1b0 <Jpour3006@gmail.com>
2022-03-08 18:20:43 +0100

    add comments

--- src/requester.ts
+++ src/requester.ts
@@ -2,9 +2,19 @@ import { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
 import { WrongCredentials } from "@";
 import { ServerError, UnknownFetchError } from "@/errors";
 
+/**
+ * the requester used to make requests to the dsbmobile-api
+ */
 export class Requester {
     constructor(public readonly axiosInstance: AxiosInstance) {}
 
+    /**
+     * get a certain resource
+     * @param uri the uri to send the request to
+     * @param config the axios-config to apply
+     * @returns the axios-response
+     * @throws `ServerError`, `WrongCredentials` or `UnknownFetchError` when something goes wrong
+     */
     public async get(uri: string, config: AxiosRequestConfig = {}) {
         try {
             return await this.axiosInstance.get(uri, config);
